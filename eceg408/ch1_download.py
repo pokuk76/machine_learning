a24b497d8b291e06aed443f66b8af5a7dc5957ac
Kofi Poku <kap029@bucknell.edu>
2020-04-06 18:58:53 -0400

    Add Files/Folders

---
+++ eceg408/ch1_download.py
@@ -0,0 +1,12 @@
+# Download the data
+import urllib.request
+import os
+
+datapath = os.path.join("datasets", "lifesat", "")
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+os.makedirs(datapath, exist_ok=True)
+for filename in ("oecd_bli_2015.csv", "gdp_per_capita.csv"):
+    print("Downloading", filename)
+    url = DOWNLOAD_ROOT + "datasets/lifesat/" + filename
+    urllib.request.urlretrieve(url, datapath + filename)
